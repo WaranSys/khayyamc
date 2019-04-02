f2595dc2cdc1325821085190f42b8cb40fdb211d
Ashkan Khaleghi <khaleqiashkan@gmail.com>
2019-04-02 18:01:27 +0430

    add keyPair to wallet

--- dev-test.js
+++ dev-test.js
@@ -1,13 +1,5 @@
-const Blockchain = require('./blockchain');
+const Wallet = require('./wallet');
 
-// const block = new Block('foo', 'bar', 'zoo', 'baz');
-// console.log(block.toString());
-// console.log(Block.genesis().toString());
+const wallet = new Wallet();
 
-// const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
-// console.log(fooBlock.toString());
-
-const bc = new Blockchain();
-for (let i=0; i<10;i++){
-    console.log(bc.addBlock(`foo${i}`).toString());
-}
+console.log(wallet.toString());
