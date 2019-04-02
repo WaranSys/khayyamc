782aecb8bb7a22f78c1f580b6b259c1204b05372
Ashkan Khaleghi <khaleqiashkan@gmail.com>
2019-04-02 19:05:39 +0430

    verify transaction unit test

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -23,6 +23,15 @@ describe('Transaction', () => {
         expect(transaction.input.amount).toEqual(wallet.balance);
     });
 
+    it('validate a valid transaction.', () => {
+        expect(Transaction.verifyTransaction(transaction)).toBe(true);
+    });
+
+    it('invalidate a corrupt transaction.', () => {
+        transaction.outputs[0].amount = 50000;
+        expect(Transaction.verifyTransaction(transaction)).toBe(false);
+    });
+
     describe('transacting with an amount that exceeds the balance', () => {
         beforeEach(() => {
             amount = 50000;
